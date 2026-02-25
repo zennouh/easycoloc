626a13df149c4b277db7cb9e9a4e7d5c6d08ac16
nouh <nouh.nowile@gmail.com>
2026-02-25 12:33:35 +0000

    edit the docker file and docker comoser

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,12 @@ import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+
     plugins: [
+        tailwindcss(),
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css' ,'resources/js/app.js'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
 });
