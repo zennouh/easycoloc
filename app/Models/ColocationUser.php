44c9258abac8d89e2320edffad1f7a83e6d2210f
nouh <nouh.nowile@gmail.com>
2026-02-27 10:00:53 +0000

    convert it to pivot instaed of normal model

--- app/Models/ColocationUser.php
+++ app/Models/ColocationUser.php
@@ -3,8 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\Pivot;
 
-class ColocationUser extends Model
+class ColocationUser extends Pivot
 {
     protected $table = 'colocation_user';
 
