5cb9ef1617578979d03a747affe5f131c6094576
nouh <nouh.nowile@gmail.com>
2026-02-27 10:10:18 +0000

    create new migration for add foregni key between settlements and expenses

---
+++ database/migrations/2026_02_26_141135_add_constraint_to_settlements.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('settlements', function (Blueprint $table) {
+            $table->foreignId('expense_id')
+                ->constrained('expenses')
+                ->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('settlements', function (Blueprint $table) {
+            //
+        });
+    }
+};
